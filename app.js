a81113606b9e6a18239be429c8f8b349a88a16e5
Eman Gamal <emangaamal@gmail.com>
2023-03-04 13:10:59 +0200

    add

---
+++ app.js
@@ -0,0 +1,40 @@
+const cors = require("cors");
+const mongoose=require("mongoose");
+const express = require("express");
+const morgan = require('morgan');
+const BookRoute=require("./Routes/BookRoute");
+
+//Server
+const server = express();
+let port = process.env.port||8080;
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/Library")
+        .then(()=>{
+            console.log("DB is connected");
+            server.listen(port,()=>{
+                console.log("Server is listenng..",port);
+            });
+        })
+        .catch(error=>{
+            console.log("Db Problem "+error);
+        })
+//////////////////
+
+server.use(cors());
+
+//First Middleware
+server.use(morgan('combined'))
+server.use(express.json());
+server.use(express.urlencoded({extended:false}));
+//Routes  
+server.use(BookRoute);
+
+//404 error not found Middleware
+server.use((request,response,next)=>{
+    response.status(404).json({message:"Not Found"});
+});
+
+//Error Middleware
+server.use((error,request,response,next)=>{
+    response.status(500).json({message:error+""})
+});
